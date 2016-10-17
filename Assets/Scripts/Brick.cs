c516b244a0a03546e31fb654e92d0eaa55dbf953
Nahuel Ruda <mezzanine510@gmail.com>
2016-10-17 14:53:19 -0700

    Added: brick + brick collision detection

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour 
+{
+
+	public int maxHits;
+
+	private int timesHit;
+
+	void Start () 
+	{
+		timesHit = 0;
+	}
+
+	void Update () 
+	{
+
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		timesHit++;
+	}
+}
